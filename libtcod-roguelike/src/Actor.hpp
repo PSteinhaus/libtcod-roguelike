9f3c078ffb46f66d49fa6710e6be343af4b21d86
Philipp Steinhaus <philipp_steinhaus@web.de>
2018-12-29 19:50:00 +0100

    chapter 2

---
+++ libtcod-roguelike/src/Actor.hpp
@@ -0,0 +1,11 @@
+class Actor
+{
+public:
+	int x,y;		// position on map
+	int ch;			// ascii code
+	TCODColor col;	// color
+
+	Actor(int x, int y, int ch, const TCODColor &col);
+	~Actor();
+	void render() const;
+};
--- libtcod-roguelike/src/main.cpp
+++ libtcod-roguelike/src/main.cpp
@@ -1,20 +1,14 @@
 #include "libtcod.hpp"
+#include "Actor.hpp"
+#include "Map.hpp"
+#include "Engine.hpp"
+
+Engine engine;
 
 int main() {
-	int playerx=40,playery=25;
-	TCODConsole::initRoot(80,50,"libtcod C++ tutorial",false,TCOD_RENDERER_GLSL);
 	while ( !TCODConsole::isWindowClosed() ) {
-		TCOD_key_t key;
-    	TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS,&key,NULL);
-    	switch(key.vk) {
-			case TCODK_UP :		playery--; break;
-			case TCODK_DOWN :	playery++; break;
-			case TCODK_LEFT :	playerx--; break;
-			case TCODK_RIGHT :	playerx++; break;
-			default:break;
-		}
-    	TCODConsole::root->clear();
-    	TCODConsole::root->putChar(playerx,playery,'@');
+		engine.update();
+		engine.render();
 		TCODConsole::flush();
 	}
 	TCOD_quit();
